97f568499694c4ea3bc5e2fad548a4ca07e68057
snerks <martin.github@outlook.com>
2018-10-11 12:50:33 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { NameEditorComponent } from "./name-editor/name-editor.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NameEditorComponent],
+  imports: [NgbModule, BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
